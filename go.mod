1b14e53e486b1dbfbb06fabc804f315545b36570
zhailianxing <575206613@qq.com>
2020-04-10 21:54:23 +0800

    向127.0.0.1:8500的consul里注册一个服务:服务名为prodService,服务地址是:127.0.0.1:8081

--- go.mod
+++ go.mod
@@ -3,10 +3,11 @@ module myGoMicro
 go 1.12
 
 require (
-	github.com/gin-gonic/gin v1.6.2 // indirect
+	github.com/gin-gonic/gin v1.6.2
 	github.com/golang/protobuf v1.3.3 // indirect
 	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/micro/go-micro v1.18.0
+	github.com/micro/go-plugins v1.5.1
 	golang.org/x/sys v0.0.0-20200116001909-b77594299b42 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- main/main.go
+++ main/main.go
@@ -2,25 +2,33 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/micro/go-micro/registry"
+	"github.com/micro/go-plugins/registry/consul"
+
 	"github.com/micro/go-micro/web"
 )
 
+//大坑啊：github.com/micro/go-micro/registry/consul用作注册中心，在1.14.0版本之后删除了，需要用之前的micro版本. 或者换成etcd作为注册中心
+//"github.com/micro/go-micro/registry/consul"
+
 func main(){
+	//1.使用 go-plugins 中内置的consul
+	//2. 127.0.0.1:8500是consul的地址。
+	consuReg := consul.NewRegistry(registry.Addrs("127.0.0.1:8500"))
 
-	// 1。原始的http服务
-	//s := web.NewService(web.Address(":8081")) // imporant  冒号。  ":8081"
-	//s.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
-	//	writer.Write([]byte("ok"))
-	//})
-	//s.Run()
-
-	// 2。引入 gin 框架实现http服务
 	ginRouter := gin.Default()
 	ginRouter.Handle("GET", "/hello", func(context *gin.Context) {
 		context.String(200, "hello moto")
 	})
-	server := web.NewService(web.Address(":8081"), web.Handler(ginRouter))
-	server.Run()
+	
+	//向consuReg所代表的cousul里，注册名字为"prodServcie"的服务，此服务的地址是"127.0.0.1:8081"
+	server := web.NewService(
+		web.Registry(consuReg),
+		web.Name("prodServcie"),
+		web.Address(":8081"), // 即：127.0.0.1:8081的简写
+		web.Handler(ginRouter),
+		)
 
+	server.Run()
 
 }
